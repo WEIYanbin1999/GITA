b034860919fd1b7bc244743420f11426a91a0ed1
ShyFoo <fus.jayce@gmail.com>
2024-05-15 14:12:07 +0800

    Update eval for fastchat

--- fastchat/custom_eval/eval.py
+++ fastchat/custom_eval/eval.py
@@ -200,7 +200,7 @@ def eval_model(args):
         conv.append_message(conv.roles[1], None)
         prompt = conv.get_prompt()
         input_ids = tokenizer([prompt]).input_ids
-        input_ids = input_ids[:, :4050]
+        input_ids = [ids[:4050] for ids in input_ids]
         
         with torch.inference_mode():
             output_ids = model.generate(
